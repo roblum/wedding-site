b184aac76eabb0477db0ca151da21e2c963b35c4
Robert <lum.robert@gmail.com>
2015-02-06 01:34:52 -0500

    dev(script): added main-container fadeout effect - css3; panTo chelsea piers light house.. will add pin for lighthouse

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-(function(){
+(function($){
+	var map;
+
+	function __init__(){
+		gMaps._init();
+		events._mainContainer();
+	}
+
+	var events = {
+		_mainContainer: function(){
+			$('.main-container').click(function(){
+				var chelsea = new google.maps.LatLng(
+									gMapsData.chelseaLocation.latitude, 
+									gMapsData.chelseaLocation.longitude
+									);
+
+					$(this).addClass('animated fadeOut');
+					map.panTo(chelsea)
+					map.setZoom(16);
+			});
+		}
+	}
 
 	var gMapsData = {
 		startLocation : {
 			latitude: 40.7238493,
 			longitude: -74.0026836
 		},
-		zoom : 13
+		zoom : 13,
+		chelseaLocation : {
+			latitude: 40.74757,
+			longitude: -74.008509,
+		},	
 	}
 
 	var gMaps = {
-		init: function(){
+		_init: function(){
 			var mapOptions = {
 	          	center: { lat: gMapsData.startLocation.latitude, lng: gMapsData.startLocation.longitude},
 	          	zoom: gMapsData.zoom,
@@ -17,13 +42,13 @@
 	          	scrollwheel: false
         	};
 
-	        var map = new google.maps.Map(document.getElementById('google-maps'), mapOptions);
+	        map = new google.maps.Map(document.getElementById('google-maps'), mapOptions);
 
 	      	// google.maps.event.addDomListener(window, 'load', gMaps.init);
 		},
 	}
 
-	google.maps.event.addDomListener(window, 'load', gMaps.init);
-})();
+	google.maps.event.addDomListener(window, 'load', __init__);
+})(jQuery);
 
         
